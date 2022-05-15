5ced2442c1778934d39486905378019c7bcc2dc4
Nicolo Tropeano <gott.tropeano@gmail.com>
2022-05-15 13:51:09 +0000

    Exercise

--- inc/ControlSystem.hpp
+++ inc/ControlSystem.hpp
@@ -5,6 +5,8 @@
 #include <eeros/core/Executor.hpp>
 #include <eeros/control/Constant.hpp>
 #include <eeros/control/Gain.hpp>
+#include <eeros/control/PeripheralInput.hpp>
+#include <eeros/control/PeripheralOutput.hpp>
 
 using namespace eeros::control;
 
@@ -14,8 +16,9 @@ public:
     ControlSystem(double dt);
 
     // Define Blocks
-    Constant<> myConstant;
-    Gain<> myGain;
+    PeripheralInput<> myquat;
+    PeripheralOutput<> myservo;
+    Gain<> mygain;
 
     TimeDomain timedomain;
 };
--- src/ControlSystem.cpp
+++ src/ControlSystem.cpp
@@ -1,24 +1,26 @@
 #include "ControlSystem.hpp"
 
 ControlSystem::ControlSystem(double dt)
-    : myConstant(1.0), myGain(2.0),
-      timedomain("Main time domain", dt, true)
+    : mygain(1.5), myquat("quat1"), myservo("servo1"), timedomain("Main time domain", dt, true)
 {
     // Name all blocks
-    myConstant.setName("My constant");
-    myGain.setName("My gain");
+    myquat.setName("Quaternion X");
+    //mygain.setName("My Gain");
 
     // Name all signals
-    myConstant.getOut().getSignal().setName("My constant value");
-    myGain.getOut().getSignal().setName("My constant value multiplied with my gain");
+    myquat.getOut().getSignal().setName("Quaternion Angle");
+    //mygain.getOut().getSignal().setName("Quaternion multiplied with gain");
+    
 
     // Connect signals
-    myGain.getIn().connect(myConstant.getOut());
+    // mygain.getIn().connect(myquat.getOut());
+    myservo.getIn().connect(myquat.getOut());
 
     // Add blocks to timedomain
-    timedomain.addBlock(myConstant);
-    timedomain.addBlock(myGain);
-
+    // timedomain.addBlock(mygain);
+    timedomain.addBlock(myquat);
+    timedomain.addBlock(myservo);
+    
     // Add timedomain to executor
     eeros::Executor::instance().add(timedomain);
 }
