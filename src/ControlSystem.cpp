d1c59fa5fb860f756e96eab19d3bb7fba8eb6a6d
Nicolo Tropeano <gott.tropeano@gmail.com>
2022-05-17 11:34:31 +0000

    Gain

--- src/ControlSystem.cpp
+++ src/ControlSystem.cpp
@@ -1,23 +1,24 @@
 #include "ControlSystem.hpp"
 
 ControlSystem::ControlSystem(double dt)
-    : mygain(1.5), myquat("quat1"), myservo("servo1"), timedomain("Main time domain", dt, true)
+    : mygain(2), myquat("quat1"), myservo("servo1"), timedomain("Main time domain", dt, true)
 {
     // Name all blocks
     myquat.setName("Quaternion X");
-    //mygain.setName("My Gain");
+    mygain.setName("My Gain");
+    myservo.setName("Servo");
 
     // Name all signals
     myquat.getOut().getSignal().setName("Quaternion Angle");
-    //mygain.getOut().getSignal().setName("Quaternion multiplied with gain");
+    mygain.getOut().getSignal().setName("Quaternion multiplied with gain");
     
 
     // Connect signals
-    // mygain.getIn().connect(myquat.getOut());
-    myservo.getIn().connect(myquat.getOut());
+    mygain.getIn().connect(myquat.getOut());
+    myservo.getIn().connect(mygain.getOut());
 
     // Add blocks to timedomain
-    // timedomain.addBlock(mygain);
+    timedomain.addBlock(mygain);
     timedomain.addBlock(myquat);
     timedomain.addBlock(myservo);
     
